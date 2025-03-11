cdde1b38e243398dd4974ebf0e20e17ea3dce3f4
David-659 <juanestebanvelez0121@gmail.com>
2025-03-11 09:18:38 -0500

    controllers configurado con el router

--- routes/router.js
+++ routes/router.js
@@ -1,53 +1,14 @@
 const exp = require('express')
 const router = exp.Router();
 const controladorProducto = require('../src/controllers/producto.controller')
+const controladorVendedor = require('../src/controllers/vendedor.controller')
 
 router.get('/productos',controladorProducto.listarProductos);
+router.get('/productos/:ref',controladorProducto.buscarProductos);
+router.post('/productos',controladorProducto.agregarProductos);
+router.put('/productos/:x',controladorProducto.editarProductos);
+router.delete('/productos/:id',controladorProducto.elimimarProductos);
 
-router.get('/productos/:ref', async (req, res) => {
-    let resultado = await modeloProducto.find({"referencia": req.params.ref });
-    console.log(resultado)
-    if (resultado) {
-        res.json(resultado)
-    }else {
-        res.json({"mensaje": "hubo un error"})
-    }
-});
-
-router.post('/productos', async (req, res) => {
-    const nuevoProducto = {
-        "referencia": req.body.referencia,
-        "nombre": req.body.nombre,
-        "precio": req.body.precio
-    }
-    
-    let insercion= await modeloProducto.create(nuevoProducto);
-    if (insercion)
-        res.status(200).json({"mensaje":"registro exitoso"})
-    else
-        res.status(404).json({"mensaje":"se presento un error"})
-});
-
-router.put('/productos/:x', async (req, res) => {
-    const productoEditado = {
-        referencia: req.params.ref,
-        nombre: req.body.nom,
-        precio: req.body.precio,
-    }
-    let actualizacion = await modeloProducto.findOneAndUpdate({referencia: req.params.x}, productoEditado);
-    if (actualizacion)
-        res.status(200).json({"mensaje":"actualización exitosa"})
-    else
-        res.status(404).json({"mensaje":"se presento un error"})
-});
-
-router.delete('/productos/:id', async (req, res) => {
-    console.log(req.params.id,req.body.ref)
-    let eliminacion = await modeloProducto.findOneAndDelete({referencia:req.params.ref});
-    if (eliminacion)
-        res.status(200).json({"mensaje":"eliminación exitosa"})
-    else
-    res.status(404).json({"mensaje":"se presento un error"})
-});
+router.get('/registrar',controladorVendedor.register);
 
 module.exports = router;
--- src/controllers/producto.controller.js
+++ src/controllers/producto.controller.js
@@ -9,3 +9,52 @@ exports.listarProductos = async (req, res) => {
         res.status(500).json({error})
     }
 };
+
+exports.buscarProductos = async (req, res) => {
+    let buscarProductos = await modeloProducto.find({"referencia": req.params.ref });
+    console.log(buscarProductos)
+    if (buscarProductos) {
+        res.status(200).json(buscarProductos)
+    }else {
+        res.status(500).json({error})
+    }
+};
+
+exports.agregarProductos = async (req, res) => {
+    const nuevoProducto = {
+        "referencia": req.body.referencia,
+        "nombre": req.body.nombre,
+        "precio": req.body.precio
+    }
+    
+    let agregarProductos = await modeloProducto.create(nuevoProducto);
+    if (agregarProductos) {
+        res.status(200).json(agregarProductos)
+    }else {
+        res.status(500).json({error})
+    }
+}
+
+exports.editarProductos = async (req, res) => {
+    const productoEditado = {
+        referencia: req.params.ref,
+        nombre: req.body.nom,
+        precio: req.body.precio,
+    }
+    let editarProductos = await modeloProducto.findOneAndUpdate({referencia: req.params.x}, productoEditado);
+    if (editarProductos) {
+        res.status(200).json(editarProductos)
+    }else {
+        res.status(500).json({error})
+    }
+}
+
+exports.elimimarProductos =  async (req, res) => {
+    console.log(req.params.id,req.body.ref)
+    let elimimarProductos = await modeloProducto.findOneAndDelete({referencia:req.params.ref});
+    if (elimimarProductos) {
+        res.status(200).json(elimimarProductos)
+    }else {
+        res.status(500).json({error})
+    }
+}
